fb0c702d497ac8b3cd69b55944d16f5aa7ff76ed
ElishaBartov <elisha.bartov@emerson.com>
2022-11-19 23:32:33 +0200

    Fixed MenuBar, Home. Users Array updated in real time. Users list not Updated in real time !!!!! need solve.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,46 +1,53 @@
+import { useState } from "react";
+import { User } from "../models/Models";
 import { Statuses } from "../types/Types";
 
 interface Props {
-    fullName: string;
-    handleFullName: Function;
-    email: string;
-    handleEmail: Function;
-    selectedStatus: string;
-    handleStatusChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-    handleButtonClick: Function;
+    updateUsers: Function;
 }
 
 const statuses = Object.values(Statuses);
 
-function MenuBar({
-        fullName,
-        handleFullName,
-        email,
-        handleEmail,
-        selectedStatus,
-        handleStatusChange,
-        handleButtonClick}: Props) {
+function MenuBar({updateUsers}: Props) {
+
+    // Menu States        
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [status,setStatus] = useState(Statuses.empty);
+
+    function handleButtonClick() {
+        // 1. Get...
+        const user: User = {
+            id: new Date().getMilliseconds(),
+            fullName: fullName,
+            email: email,
+            status: status
+        }     
+        // 2. Update users Array by setUsers state function
+        updateUsers(user);
+    }
+
     return (  
         <div className="container d-flex p-4 justify-content-between">
             <h4>User:</h4>
             <div className="d-flex">
                 <input
                     value={fullName}    
-                    onChange={(e) => handleFullName(e)}
+                    onChange={(e) => setFullName(e.target.value)}
                     className="form-control mx-2"
                     type="text"
                     placeholder="Full Name"
                 ></input>
                 <input
                     value={email}
-                    onChange={(e) => handleEmail(e)}
+                    onChange={(e) => setEmail(e.target.value)}
                     className="form-control mx-2"
                     type="email"
                     placeholder="Email Address"
                 ></input>
                 <select
-                    value={selectedStatus}
-                    onChange={handleStatusChange}
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value as Statuses)}
                     className="form-select mx-2">
                     {
                         statuses.map((status) => 
@@ -53,7 +60,7 @@ function MenuBar({
                     }    
                 </select>
                 <button 
-                    onClick={() => handleButtonClick()}
+                    onClick={handleButtonClick}
                     type="button" 
                     className="btn btn-info">Add</button>
             </div>            
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,97 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Footer from "../components/Footer";
 import MenuBar from "../components/MenuBar";
 import Table from "../components/Table";
 import Title from "../components/Title";
 import { User } from "../models/Models";
-import { Statuses } from "../types/Types";
-
-// const data = [
-//     {
-//         id: 1,
-//         fullName: "Elisha Bartov",
-//         status: Statuses.active,
-//         email: "elisha.bartov@emerson.com"
-//     },
-//     {
-//         id: 2,
-//         fullName: "Elisha Bartov",
-//         status: Statuses.active,
-//         email: "alishe.b@gmail.com"
-//     },
-//     {
-//         id: 3,
-//         fullName: "Renata Fayziev",
-//         status: Statuses.expired,
-//         email: "renatulya.f@gmail.com"
-//     },
-//     {
-//         id: 4,
-//         fullName: "Evelin Bartov",
-//         status: Statuses.banded,
-//         email: "evelin.b@gmail.com"
-//     }
-// ];
 
 function Home() {
     // App States
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
-    const [status,setStatus] = useState(Statuses.empty);
     const [users,setUsers] = useState<Array<User>>([]);
-    // const [users,setUsers] = useState([...data]);
 
-    function handleFullName(element: React.ChangeEvent<HTMLInputElement>) {
-        // 1. Get Value from "Full Name" Input
-        const value = element.target.value;       
-        // 2. Update fullName
-        console.log(value);
-        setFullName(value); 
-    }
-
-    function handleEmail(element: React.ChangeEvent<HTMLInputElement>) {
-        // 1. Get Value from "Email" input
-        const value = element.target.value;
-        // 2. Update email
-        console.log(value);
-        setEmail(value);
-    }
-
-    function handleStatusChange(element: React.ChangeEvent<HTMLSelectElement>) {
-        // 1. Get Status from "Status" select input
-        const value = element.target.value as Statuses;
-        // 2. Update status
-        console.log(value);
-        setStatus(value);
-    }
-
-    function handleButtonClick() {
-        // 1. Get...
-        let user: User = {
-            id: new Date().getMilliseconds(),
-            fullName: fullName,
-            email: email,
-            status: status
-        }        
+    function updateUsers(user: User) {
         users.push(user);
-        
-        // 2. Update users
         setUsers(users);
     }
 
     return (
         <>
-            <Title content="Add User"/>
-            <MenuBar
-                fullName={fullName}
-                handleFullName={handleFullName}
-                email={email}
-                handleEmail={handleEmail}
-                selectedStatus={status}
-                handleStatusChange={handleStatusChange}
-                handleButtonClick={handleButtonClick} 
-            />
-            <Title content="Users List"/>  
+            <Title content="Add User" />
+            <MenuBar 
+                updateUsers={updateUsers} />
+            <Title content="Users List" />  
             {
                 // Condition rendering
                 // Example x.length === 0 ? 'Yes' : 'No'                
